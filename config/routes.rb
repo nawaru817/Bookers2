5487b12fd332297c9d03b07facd2fa8f30a9b636
nawaru817 <t010mbharada@gmail.com>
2021-02-26 06:17:58 +0000

    [Finish] Bookers2

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   get 'users' => 'users#users'
   get 'users/show'
   get 'home/about' => 'homes#about'
-  #post 'books/:id' => 'books#create'
-
+  
   devise_for :users
   root to: 'homes#top'
   resources :books, only: [:create, :edit, :update, :index, :show, :destroy]
